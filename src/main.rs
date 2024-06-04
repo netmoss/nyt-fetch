4d0778209115ec50c2907f75e4c29a16664febb2
ptblank <ptblank@pm.me>
2024-06-05 00:11:32 +1000

    Added Mini function

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn wordle() -> Result<String, Box<dyn Error>> {
     Ok(word.solution.to_uppercase())
 }
 
-fn connections() -> Result<(), Box<dyn Error>> {
+fn connections() -> Result<String, Box<dyn Error>> {
     let date: String = Local::now().to_string();
 
     #[derive(Deserialize)]
@@ -32,13 +32,14 @@ fn connections() -> Result<(), Box<dyn Error>> {
     let data: Connections = response.json()?;
 
     let difficulties = ["Straightforward", "Intermediate", "Hard", "Tricky"];
+    let mut result_string = String::new();
     for (i, (group_name, group)) in data.groups.iter().enumerate() {
-        println!("Difficulty: {}", difficulties[i]);
-        println!("{}:", group_name);
-        println!("{}\n", group.members.join(", "));
+        result_string.push_str(&format!("Difficulty: {}\n", difficulties[i]));
+        result_string.push_str(&format!("{}:\n", group_name));
+        result_string.push_str(&format!("{}\n\n", group.members.join(", ")));
     }
 
-    Ok(())
+    Ok(result_string)
 }
 
 fn strands() -> Result<String, Box<dyn Error>> {
@@ -57,9 +58,50 @@ fn strands() -> Result<String, Box<dyn Error>> {
 
 }
 
+fn mini() -> Result<String, Box<dyn Error>> {
+
+    #[derive(Deserialize)]
+    struct Puzzle {
+        body: Vec<Body>,
+    }
+ 
+    #[derive(Deserialize)]
+    struct Body {
+        cells: Vec<Cell>,
+    }
+    
+    #[derive(Deserialize)]
+    struct Cell {
+        #[serde(default)] // make clues field optional
+        clues: Option<Vec<u8>>,
+        answer: Option<String>,
+    }
+   
+    let response = reqwest::blocking::get("https://www.nytimes.com/svc/crosswords/v6/puzzle/mini.json")?;
+    let data: Puzzle = response.json()?;
+
+    let mut answers_list: Vec<String> = Vec::new();
+    let mut row = 0;
+
+    for cell in data.body[0].cells.iter() {
+        if let Some(answer) = &cell.answer {
+            if let Some(clues) = &cell.clues {
+                if !clues.is_empty() && clues[0] > row {
+                    row += 1;
+                    answers_list.push(format!("\n{}", answer));
+                } else {
+                    answers_list.push(answer.clone());
+                }
+            }
+        }
+    }
+
+    Ok(format!("Solutions across:\n{}", answers_list.concat()))
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     loop {
-        println!("Enter the game you want the answers to (Wordle, Connections, Strands, Mini), or type 'quit' to close the program.");
+        println!("Enter the game you want the answers to (Wordle, Connections, Strands, Mini), or enter 'quit' to close the program.");
         let mut selection = String::new();
         io::stdin().read_line(&mut selection)?;
         
@@ -68,10 +110,13 @@ fn main() -> Result<(), Box<dyn Error>> {
                 println!("Today's solution is: {}", wordle()?);
             }
             "connections" => {
-               connections()?;
+               println!("{}", connections()?);
             }
             "strands" => {
                 println!("{}", strands()?);
+            }
+            "mini" => {
+                println!("{}", mini()?);
             }
             "quit" => {
                 exit(0)
